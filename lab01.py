85f529160771d297ca3b64067baf4d5b94b69f87
U-Justin-PC\Justin <Justin@Justin-PC.(none)>
2013-04-23 11:13:07 -0500

    old

--- lab01.py
+++ lab01.py
@@ -7,39 +7,64 @@ import socket
 import threading
 import Queue
 import signal
-import time
-import math
 
 FORMAT = '%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s'
 DATE_FORMAT = '%H%M%S'
+DEFAULT_VELOCITY = 100 # velocity in mm/s
 
-
-def RoundCorner():
-	robot.Drive(100, -200)
-	time.sleep(2.85)
-	robot.Stop()
 	
 def Turn(angle, dir):
-	robot.TurnInPlace(100, dir)
-	time.sleep(8.5*angle/360)
+	robot.TurnInPlace(DEFAULT_VELOCITY, dir)
+	time.sleep(8.3*angle/360)
 	robot.Stop()
+	print "turned"
+
+def moveBodyLength():
+	robot.DriveStraight(DEFAULT_VELOCITY)
+	start = time.time()
+	while (time.time()-start < 3):
+		if(robot.sensors.GetBump() == 1):
+			onBump()
+			return False
+	robot.Stop()
+	print "moved body length"
+	return True
+
+def DriveUntilWallDetected():
+	robot.DriveStraight(DEFAULT_VELOCITY)
+
+	while(robot.sensors.GetWall() == 0):
+		pass
+
+	robot.Stop()
+	print "found wall"
+	
+def onBump():
+	print "bumped"
+	robot.Stop()
+	Turn(90, "ccw")
+	robot.DriveStraight(DEFAULT_VELOCITY)
+	
+def onLostWall():
+	print "lost wall"
+	if(moveBodyLength()):
+		Turn(90, "cw")
+		DriveUntilWallDetected()
+	robot.DriveStraight(DEFAULT_VELOCITY)
 
 def main():
 	logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt=DATE_FORMAT) 
 	global robot
 	robot = Rtbot(sys.argv[1])
 	robot.start()
-	
-	while True:
-		robot.DriveStraight(100)
-		if(robot.sensors.GetBump() == 1):
-			robot.Stop()
-			Turn(90, "ccw")
-		if(robot.sensors.GetWall() == 0):
-			robot.Stop()
-			RoundCorner()
-	
-  
 
+	robot.DriveStraight(DEFAULT_VELOCITY)
+		
+	while True:
+		if(robot.sensors.GetBump() == 1):
+			onBump()
+		if(robot.sensors.GetWall() == 0):
+			onLostWall()
+			
 if __name__ == '__main__':
   main()  
